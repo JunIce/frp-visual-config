2af80c0cada66a47bda8e49d0b31ba3e2824e504
volcan0 <east.qrj@gmail.com>
2023-07-08 06:36:23 +0800

    fix: fix assets error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import App from './App.vue';
 import 'element-plus/dist/index.css';
-import './assets/css/icon.css';
 
 const app = createApp(App);
 app.use(createPinia());
